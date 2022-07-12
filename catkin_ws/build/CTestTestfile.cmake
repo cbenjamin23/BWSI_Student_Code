0be522061b54071efd49727cd0752203e0308638
cbenjamin23 <chbenj36@gmail.com>
2022-07-12 17:41:17 -0400

    End of day 2 commits

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/charlie/BWSI_Student_Code/catkin_ws/src
+# Build directory: /home/charlie/BWSI_Student_Code/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ code_along_pep8/code_along.py
@@ -0,0 +1,12 @@
+import numpy
+import matplotlib.pyplot as plt
+
+
+#2 lines before new function
+def  long_function_name(var_one, var_two, var_three
+			var_four, var_five, var_six) #indented until it matches with the end of the last line
+
+class CamelCase:
+
+	def __init__(self):
+		self._private = "my private variable"
